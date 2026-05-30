67db3007e612502f7fb4c42193792638295b3847
J0ss077 <jossmilo133@gmail.com>
2026-05-30 11:24:16 -0500

    fix

--- compatibilities/data-stage-03/5dim-compatibility.lua
+++ compatibilities/data-stage-03/5dim-compatibility.lua
@@ -1,3 +1,3 @@
-if mods["5dim_compatibility"] then return end
+if not mods["5dim_compatibility"] then return end
 
 if mods["Krastorio2"] then require("scripts.others.fixes").reset_collision_masks() end
