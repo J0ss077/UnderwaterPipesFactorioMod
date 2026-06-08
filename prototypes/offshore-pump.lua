63ba378da725d97533fe997ae6fa92a263138dba
J0ss077 <jossmilo133@gmail.com>
2026-06-08 12:29:18 -0500

    test

--- prototypes/offshore-pump.lua
+++
@@ -1,8 +0,0 @@
-log(serpent.block(data.raw["offshore-pump"]["offshore-pump"]))
-
-data:extend({
-    {
-        type = "offshore-pump",
-        name = "F077UP-high-pressure-pump",
-    }
-})
---
+++ prototypes/pressure-pump.lua
@@ -0,0 +1,9 @@
+log(serpent.block(data.raw["pump"]["pump"]))
+
+data:extend({
+    {
+        type = "pump",
+        --
+        name = "F077UP-pressure-pump",
+    }
+})
