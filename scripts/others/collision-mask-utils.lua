abe02fb56f550367aa55f41a30a0386795267dc6
J0ss077 <jossmilo133@gmail.com>
2026-06-12 12:38:30 -0500

    1.2.8

--- scripts/others/collision-mask-utils.lua
+++ scripts/others/collision-mask-utils.lua
@@ -4,7 +4,7 @@ local definitions = require("scripts.var.definitions")
 
 --- @param filter string?
 ---
---- @return string|function
+--- @return (string|function)?
 ---
 local function iterate_underwater_pipes(filter)
 
@@ -14,7 +14,7 @@ local function iterate_underwater_pipes(filter)
 
         index = index + 1
 
-        local name = data_carrier.data.underwater_entities[index]; if name == nil then return nil end
+        local name = data_carrier.data.underwater_entities[index]; if name == nil then return nil, nil end
 
         local type = data.raw["pipe"][name] and "pipe" or "pipe-to-ground"
 
@@ -32,7 +32,7 @@ function module.reset_collision_masks()
     ---
     for __, entity in iterate_underwater_pipes() do
         --
-        entity.collision_mask = definitions.base_collision_mask
+        if entity then entity.collision_mask = definitions.base_collision_mask end
     end
 end
 
